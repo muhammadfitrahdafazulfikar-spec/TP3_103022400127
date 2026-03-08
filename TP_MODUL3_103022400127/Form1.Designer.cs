ed99faee130f22c543b01ba31e11f5ef3c617ffc
dafa <dafa>
2026-03-09 01:58:36 +0700

    kalkulator

---
+++ TP_MODUL3_103022400127/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace TP_MODUL3_103022400127
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            btnSamaDengan = new Button();
+            btnTambah = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(335, 90);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            label1.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(236, 139);
+            button1.Name = "button1";
+            button1.Size = new Size(57, 24);
+            button1.TabIndex = 2;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(326, 139);
+            button2.Name = "button2";
+            button2.Size = new Size(57, 24);
+            button2.TabIndex = 3;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(411, 139);
+            button3.Name = "button3";
+            button3.Size = new Size(57, 24);
+            button3.TabIndex = 4;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(236, 179);
+            button4.Name = "button4";
+            button4.Size = new Size(57, 24);
+            button4.TabIndex = 5;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(326, 179);
+            button5.Name = "button5";
+            button5.Size = new Size(57, 24);
+            button5.TabIndex = 6;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(411, 179);
+            button6.Name = "button6";
+            button6.Size = new Size(57, 24);
+            button6.TabIndex = 7;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(236, 221);
+            button7.Name = "button7";
+            button7.Size = new Size(57, 24);
+            button7.TabIndex = 8;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(326, 221);
+            button8.Name = "button8";
+            button8.Size = new Size(57, 24);
+            button8.TabIndex = 9;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(411, 221);
+            button9.Name = "button9";
+            button9.Size = new Size(57, 24);
+            button9.TabIndex = 10;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(326, 251);
+            button10.Name = "button10";
+            button10.Size = new Size(57, 24);
+            button10.TabIndex = 11;
+            button10.Text = "0";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // btnSamaDengan
+            // 
+            btnSamaDengan.Location = new Point(411, 251);
+            btnSamaDengan.Name = "btnSamaDengan";
+            btnSamaDengan.Size = new Size(57, 24);
+            btnSamaDengan.TabIndex = 12;
+            btnSamaDengan.Text = "=";
+            btnSamaDengan.UseVisualStyleBackColor = true;
+            btnSamaDengan.Click += btnSamaDengan_Click;
+            // 
+            // btnTambah
+            // 
+            btnTambah.Location = new Point(236, 251);
+            btnTambah.Name = "btnTambah";
+            btnTambah.Size = new Size(57, 24);
+            btnTambah.TabIndex = 13;
+            btnTambah.Text = "+";
+            btnTambah.UseVisualStyleBackColor = true;
+            btnTambah.Click += btnTambah_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnTambah);
+            Controls.Add(btnSamaDengan);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button btnSamaDengan;
+        private Button btnTambah;
+    }
+}
---
+++ TP_MODUL3_103022400127/Form1.cs
@@ -0,0 +1,75 @@
+namespace TP_MODUL3_103022400127
+{
+    public partial class Form1 : Form
+    {
+        private int total = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text += "1";
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text += "2";
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text += "3";
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Text += "4";
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label1.Text += "5";
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label1.Text += "6";
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            label1.Text += "7";
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+            label1.Text += "8";
+        }
+        private void button9_Click(object sender, EventArgs e)
+        {
+            label1.Text += "9";
+        }
+        private void button10_Click(object sender, EventArgs e)
+        {
+            label1.Text += "0";
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTambah_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(label1.Text))
+            {
+                total += int.Parse(label1.Text); // Menyimpan angka pertama
+                label1.Text = ""; // Mengosongkan layar untuk angka berikutnya
+            }
+        }
+
+        private void btnSamaDengan_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(label1.Text))
+            {
+                total += int.Parse(label1.Text); // Menambahkan angka terakhir
+                label1.Text = total.ToString(); // Menampilkan hasil akhir
+                total = 0; // Reset agar bisa menghitung ulang dari nol
+            }
+        }
+    }
+}
